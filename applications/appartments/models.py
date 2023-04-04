c4678804e3ed7da0708de63e3377d7a6058fbeeb
Iskender <ilispo4321@gmail.com>
2023-04-04 19:56:21 +0600

    ilya_chandges

--- applications/appartments/models.py
+++ applications/appartments/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator,MinValueValidator
 
-# Create your models here.
+
 User = get_user_model()
 
-# class Apartments(models.Model):
 
+
+
+
+# Это написал Ильяс 
